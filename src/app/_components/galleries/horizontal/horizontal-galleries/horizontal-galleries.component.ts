3375837b17d92e96aad588e94b16d593d71b4192
Zephyrin Damortien <damortien@gmail.com>
2020-08-12 08:12:52 +0200

    Improve home page for desktop, galleries are now scrollable in horizontal

--- src/app/_components/galleries/horizontal/horizontal-galleries/horizontal-galleries.component.ts
+++ src/app/_components/galleries/horizontal/horizontal-galleries/horizontal-galleries.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { GalleryService } from './../../../../_services/gallery/gallery.service';
+import { Component, OnInit, Input } from '@angular/core';
+
+interface ServiceInterface {
+  edit: boolean;
+}
 
 @Component({
   selector: 'app-horizontal-galleries',
@@ -6,8 +11,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./horizontal-galleries.component.scss']
 })
 export class HorizontalGalleriesComponent implements OnInit {
-
-  constructor() { }
+  @Input() service: ServiceInterface;
+  constructor(public galleries: GalleryService) { }
 
   ngOnInit(): void {
   }
